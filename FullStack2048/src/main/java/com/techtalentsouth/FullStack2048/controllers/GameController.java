48d319064975b31feadb7b3f7bca22914d277c42
Xero1000 <alex.heil@comcast.net>
2023-04-27 09:16:51 -0400

    Added GetMapping and PostMapping functions for highscores

--- FullStack2048/src/main/java/com/techtalentsouth/FullStack2048/controllers/GameController.java
+++ FullStack2048/src/main/java/com/techtalentsouth/FullStack2048/controllers/GameController.java
@@ -1,10 +1,15 @@
 package com.techtalentsouth.FullStack2048.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.techtalentsouth.FullStack2048.models.UserHighscore;
 import com.techtalentsouth.FullStack2048.repository.GameRepository;
 
 @RestController
@@ -16,5 +21,16 @@ public class GameController {
 	@GetMapping("/test")
 	public String endpoint() {
 		return "Hello friend";
+	}
+	
+	@GetMapping("/highscores")
+	public List<UserHighscore> getHighscores() {
+		List<UserHighscore> highscores = gameRepository.findAll();
+		return highscores;
+	}
+	
+	@PostMapping("/highscores")
+	public void addHighscore(@RequestBody UserHighscore highscore) {
+		gameRepository.save(highscore);
 	}
 }
